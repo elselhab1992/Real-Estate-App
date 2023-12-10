911299bc2223e88b1aa01cee7d4514ac54766250
Mohamed Elselhab <momanchester1992@gmail.com>
2023-12-10 17:07:16 +0000

    uninstall react-bootstrap

--- src/components/HouseContext/HouseContext.js
+++ src/components/HouseContext/HouseContext.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable array-callback-return */
 /* eslint-disable react/prop-types */
 import { useState, useContext, createContext, useEffect } from "react";
 import { housesData } from "../../data";
